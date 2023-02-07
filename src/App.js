55215173a051cd50fdee332af82679ea6d52d998
yeooong-dev <yeooong12@naver.com>
2023-02-08 02:44:59 +0900

    finish

--- src/App.js
+++ src/App.js
@@ -85,10 +85,15 @@ const App = () => {
     return(
       <div key = {item.id} className='m_style'>
         {/* { item.isDone === false ? `${item.title} - ${item.memo}` : <del>{item.title} - {item.memo}</del>} */}
+        <div className='todotitle'>
         {item.title}-{item.memo}
-        <button className='delete_btn' onClick={()=> removerFuction(item.id)}>삭제</button>
-        {item.isDone === false ? <button className='clear_btn' onClick={()=> toggleFuction(item.id)}>완료</button> : <button className='clear_btn' onClick={()=> toggleFuction(item.id)}>취소</button>}
-       
+        </div>
+        <div className='buttonbox'>
+          <button className='delete_btn' onClick={()=> removerFuction(item.id)}>삭제</button>
+          {item.isDone === false ? <button className='clear_btn' onClick={()=> toggleFuction(item.id)}>완료</button> : <button className='clear_btn' onClick={()=> toggleFuction(item.id)}>취소</button>}
+        </div>
+        
+        
       </div>
     );
   };
