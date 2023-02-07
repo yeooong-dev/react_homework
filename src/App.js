57dd2a73cd889b308ec81f1eae9bd28cb78d9995
yeooong-dev <yeooong12@naver.com>
2023-02-08 01:30:44 +0900

    finish

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 
 const App = () => {
   const [users, setUsers] = useState([
-
+      // {
+      //   id : 0,
+      //   title : "title",
+      //   memo : "memo",
+      //   isDone : false, 
+      // }
   ]);
 
   const [title, setTitle] = useState('');
@@ -15,6 +20,12 @@ const App = () => {
   const memoAddHandler = (event) => {
     setMemo(event.target.value);
   }
+  //toggle button
+  const clickToggleButtonHandler = (id) => {
+    const newUser = users.map((user)=> user.id === id ? {...user,isDone : !user.isDone}:user)
+    setUsers(newUser);
+  }
+
   //삭제 버튼
   const clickRemoveButtonHandler = (id) => {
     const newUser = users.filter((user)=> user.id !== id);
@@ -27,46 +38,57 @@ const App = () => {
       id : users.length + 1,
       title : title,
       memo : memo,
-      
+      isDone : false,
     }
-
+    setUsers([...users, newUser])
+    setMemo('')
+    setTitle('')
+  }
   //완료 버튼
   const clickClearButtonHandler = (id) => {
     setUsers((users)=> users.map((newUser)=>
         newUser.id === id ? {...newUser, checked : !newUser.checked } : newUser
     ));
   };
-    setUsers([...users, newUser])
-  }
+   
 
   return (
     <div>
       <h1 className='todo_title'>My Todo List</h1>
       <div className='a_style'>
-          <input type='text' placeholder='제목' value={title} onChange={titleAddHandler} />
+          <input className='title_style' type='text' placeholder='제목' value={title} onChange={titleAddHandler} />
 
-          <input type='text' placeholder='내용' value={memo} onChange = {memoAddHandler} />
+          <input className='subtitle_style' type='text' placeholder='내용' value={memo} onChange = {memoAddHandler} />
 
           <button onClick={clickAddButtonHandler}>추가하기</button>
       </div>
 
       <div className='center'>
-        <div className='m_style'>
-          {users.map(function(item){
-          return <User key={item.id} item = {item} removerFuction = {clickRemoveButtonHandler} />;
+        <div className='m'>
+          <h1>Doing</h1>
+          {users.filter(v=>v.isDone === false).map(function(item){
+          return <User className="bg_style" key={item.id} item = {item} removerFuction = {clickRemoveButtonHandler} toggleFuction = {clickToggleButtonHandler} />;
+          
+          })}
+          <h1>Done</h1>
+          {users.filter(v=>v.isDone === true).map(function(item){
+          return <User className="bg_style" key={item.id} item = {item} removerFuction = {clickRemoveButtonHandler} toggleFuction = {clickToggleButtonHandler} />;
+          
           })}
         </div>
       </div>
     </div>
   )
 }
-  const User = ({item, removerFuction})=> {
+  const User = ({item, removerFuction, toggleFuction})=> {
 
     return(
       <div key = {item.id} className='m_style'>
-        {item.title} - {item.memo}
+        {/* { item.isDone === false ? `${item.title} - ${item.memo}` : <del>{item.title} - {item.memo}</del>} */}
+        {item.title}-{item.memo}
         <button className='delete_btn' onClick={()=> removerFuction(item.id)}>삭제</button>
-        <button className='clear_btn' onClick={()=> removerFuction(item.id)}>완료</button>
+        {item.isDone === false ? <button className='clear_btn' onClick={()=> toggleFuction(item.id)}>완료</button> : <button className='clear_btn' onClick={()=> toggleFuction(item.id)}>취소</button>}
+       
       </div>
     );
   };
